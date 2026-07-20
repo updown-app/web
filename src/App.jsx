ffe2b74af3195a2cc8f734de7c7773ed79529180
Abdullah-al-Mustafa <messagetoupdown.hq@gmail.com>
2026-07-20 22:38:25 +0600

    Add ProductIntroSection to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Layout from './components/Layout'
 import HeroSection from './sections/HeroSection'
+import ProductIntroSection from './sections/ProductIntroSection'
 
 export default function App() {
   return (
     <Layout>
       <HeroSection />
+      <ProductIntroSection />
     </Layout>
   )
 }
