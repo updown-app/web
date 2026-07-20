a98d83d7cae53746c6ea48849c68a6ab750b795d
Abdullah-al-Mustafa <messagetoupdown.hq@gmail.com>
2026-07-20 22:39:03 +0600

    Add FeaturesSection to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Layout from './components/Layout'
 import HeroSection from './sections/HeroSection'
 import ProductIntroSection from './sections/ProductIntroSection'
+import FeaturesSection from './sections/FeaturesSection'
 
 export default function App() {
   return (
     <Layout>
       <HeroSection />
       <ProductIntroSection />
+      <FeaturesSection />
     </Layout>
   )
 }
